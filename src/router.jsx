75ee1e08a8f0553c63048d1e8df42ea2d49fd7c0
erica0321 <hyunahrho29@gmail.com>
2024-11-28 01:14:42 +0900

    fix: router import 주소 수정

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import { Splash } from "./pages/splash";
-import { Letter } from "./pages/letter";
-import { Vote } from "./pages/vote";
+import { Home } from "./pages/Home";
+import { Splash } from "./pages/Splash";
+import { Letter } from "./pages/Letter";
+import { Vote } from "./pages/Vote";
 
 export const Router = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<Splash />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/letter" element={<Letter />} />
-            <Route path="/vote" element={<Vote />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Splash />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/letter" element={<Letter />} />
+      <Route path="/vote" element={<Vote />} />
+    </Routes>
+  );
 };
